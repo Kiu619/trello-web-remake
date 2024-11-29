f3ab73c9ef6d95a6228fbdca785c81c1a25fbdf3
kingk <kingk@DESKTOP-Q0U2A5C>
2024-11-30 02:53:43 +0700

    deploy on Vercel testing v1.1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,19 @@
+let apiRoot = ''
 
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8011'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trelle-api-remake.onrender.com'
+}
 // export const API_ROOT = 'http://localhost:8011';
-export const API_ROOT = 'https://trelle-api-remake.onrender.com';
+export const API_ROOT = apiRoot
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
 
 export const CARD_MEMBER_ACTIONS = {
-    ADD: 'ADD',
-    REMOVE: 'REMOVE'
+  ADD: 'ADD',
+  REMOVE: 'REMOVE'
 }
